9caede26a801d5156d65b8cf9e4b9058eeeb9045
Sebastian Higuita <sebastianhc@kometsales.com>
2019-12-26 15:18:51 -0500

    Adjustment to set env variables to PDN environment

--- src/utils/CreateTypeOrmConnection.ts
+++ src/utils/CreateTypeOrmConnection.ts
@@ -1,7 +1,26 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection, getConnectionOptions, ConnectionOptions } from 'typeorm';
 
 export const createTypeOrmConnection = async () => {
     const environment = process.env.NODE_ENV || "dev";
-    const connectionOptions = await getConnectionOptions(environment);
+    let connectionOptions: ConnectionOptions
+    //let connectionOptions: ConnectionOptions = await getConnectionOptions(environment);
+
+    if (environment === "dev" || environment === "test") {
+        connectionOptions = await getConnectionOptions(environment);
+    } else if (environment === "production") {
+        connectionOptions = Object.assign({
+            name: environment,
+            type: process.env.TYPEORM_CONNECTION,
+            host: process.env.TYPEORM_HOST,
+            port: process.env.TYPEORM_PORT,
+            username: process.env.TYPEORM_USERNAME,
+            password: process.env.TYPEORM_PASSWORD,
+            database: process.env.TYPEORM_DATABASE,
+            ssl: true
+        });
+    }
+
+    console.log(connectionOptions);
+
     return createConnection({ ...connectionOptions, name: "default" });
 }
